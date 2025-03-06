ca8827c0c18ef58c6cc84564f45df9917ffe2767
Enri238 <enrihomipat@icloud.com>
2025-03-06 12:06:38 +0100

    Updated GrupoTest

--- src/test/java/clubdeportivo/GrupoTest.java
+++ src/test/java/clubdeportivo/GrupoTest.java
@@ -1,15 +1,32 @@
 package clubdeportivo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GrupoTest {
 
     private Grupo g;
 
+    // Arrange
     @BeforeEach
     public void init() throws ClubException {
         g=new Grupo("1","Fútbol", 10, 0, 20);
+    }
+
+    @Test
+    @DisplayName("Actualizar plazas cambia correctamente el valor correspondiente")
+    public void actualizarPlazas_NuevoValor_ValorActualizado() throws ClubException {
+        // Arrange
+        int n = 9;
+        
+        // Act
+        g.actualizarPlazas(n);
+
+        // Assert
+        assertEquals(n, g.getPlazas());
     }
 
     @Test
@@ -17,4 +34,7 @@ public class GrupoTest {
         int p = g.plazasLibres();
 
     }
+
+    
+
 }
