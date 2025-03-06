afcf771348af9abdad8e97f9ef99cd7ec7f7a114
Eduardo <earizaabad@gmail.com>
2025-03-06 19:26:55 +0100

    club

--- src/test/java/clubdeportivo/ClubDeportivoTest.java
+++ src/test/java/clubdeportivo/ClubDeportivoTest.java
@@ -1,5 +1,15 @@
 package clubdeportivo;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class ClubDeportivoTest {
+
+    private ClubDeportivo c;
+
+    @BeforeEach
+    public void init() throws ClubException{
+         c = new ClubDeportivo("Futboleros");
+    }
+
 
 }
