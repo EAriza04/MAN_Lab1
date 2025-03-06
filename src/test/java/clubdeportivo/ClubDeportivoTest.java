b6076afacbc81103ce81cd65145845aa45dfa918
Enri238 <enrihomipat@icloud.com>
2025-03-06 19:32:50 +0100

    Constructor ClubDeportivo Negativo

--- src/test/java/clubdeportivo/ClubDeportivoTest.java
+++ src/test/java/clubdeportivo/ClubDeportivoTest.java
@@ -1,5 +1,10 @@
 package clubdeportivo;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.BeforeEach;
 
 public class ClubDeportivoTest {
@@ -11,5 +16,14 @@ public class ClubDeportivoTest {
          c = new ClubDeportivo("Futboleros");
     }
 
+    @Test
+    @DisplayName("Crear un club con valor negativo eleva una excepción")
+    public void constructor_ValorNegativo_LanzaClubException() {
+        // Arrange
+        int n = -1;
+
+        // Act + Assert
+        assertThrows(ClubException.class, () -> new ClubDeportivo("Miembros negativos", n));
+    }
 
 }
