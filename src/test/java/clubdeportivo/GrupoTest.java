ee198c84961409921311653c83af0fec082841af
Eduardo <earizaabad@gmail.com>
2025-03-06 17:11:20 +0100

    TestGrupo
    100% instructions coverage
    90% branches coverage

--- src/test/java/clubdeportivo/GrupoTest.java
+++ src/test/java/clubdeportivo/GrupoTest.java
@@ -1,7 +1,9 @@
 package clubdeportivo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +17,79 @@ public class GrupoTest {
     @BeforeEach
     public void init() throws ClubException {
         g = new Grupo("1","Fútbol", 10, 3, 20);
+    }
+
+    @Test
+    @DisplayName("Crear un grupo con valores incorrectos eleva una excepción")
+    public void constructor_ValorIncorrecto_LanzaClubException() throws ClubException {
+        // Arrange
+        int nplazas= 1;
+        int matriculados = 1;
+        int tarifa = -1;
+
+        // Act + Assert
+        assertThrows(ClubException.class, () -> new Grupo("1", "Fútbol", nplazas, matriculados, tarifa));
+    }
+
+    @Test
+    @DisplayName("Crear un grupo con más matriculados que plazas eleva una excepción")
+    public void constructor_MasPlazasQueMatriculados_LanzaClubException() throws ClubException {
+        // Arrange
+        int nplazas= 1;
+        int matriculados = 2;
+
+        // Act + Assert
+        assertThrows(ClubException.class, () -> new Grupo("1", "Fútbol", nplazas, matriculados, 1));
+    }
+
+    @Test
+    @DisplayName("El método getCodigo devuelve el código")
+    public void getCodigos_DevuelveCodigo() {
+        // Arrange
+        String codigoEsperado = "1";
+
+        // Act + Assert
+        assertEquals(codigoEsperado, g.getCodigo());
+    }
+
+    @Test
+    @DisplayName("El método getActividad devuelve la actividad")
+    public void getActividad_DevuelveActividad() {
+        // Arrange
+        String actividadEsperada = "Fútbol";
+
+        // Act + Assert
+        assertEquals(actividadEsperada, g.getActividad());
+    }
+
+    @Test
+    @DisplayName("El método getPlazas devuelve las plazas")
+    public void getPlazas_DevuelvePlazas() {
+         // Arrange
+         int plazasEsperadas = 10;
+
+        // Act + Assert
+        assertEquals(plazasEsperadas, g.getPlazas());
+    }
+
+    @Test
+    @DisplayName("El método getPlazas devuelve los matriculados")
+    public void getMatriculados_DevuelveMatriculados() {
+        // Arrange
+        int matriculadosEsperados = 3;
+
+        // Act + Assert
+        assertEquals(matriculadosEsperados, g.getMatriculados());
+    }
+
+    @Test
+    @DisplayName("El método getPlazas devuelve la tarifa")
+    public void getTarifa_DevuelveTarifa() {
+        // Arrange
+        int tarifaEsperada = 20;
+
+        // Act + Assert
+        assertEquals(tarifaEsperada, g.getTarifa());
     }
 
     @Test
@@ -44,7 +119,7 @@ public class GrupoTest {
 
     @Test
     @DisplayName("Matricular personas aumenta el número de matriculados correctamente")
-    public void Matricular_PersonaNueva_Anyadematriculado() throws ClubException{
+    public void matricular_PersonaNueva_Anyadematriculado() throws ClubException{
         // Arrange
         int matriculadosAntes = g.getMatriculados();
         int Nmatricular = 5;
@@ -53,9 +128,52 @@ public class GrupoTest {
         g.matricular(Nmatricular);
 
         // Assert
-        assertEquals(g.getMatriculados(), matriculadosAntes+Nmatricular);
+        assertEquals(matriculadosAntes+Nmatricular, g.getMatriculados());
     }
 
+    @Test
+    @DisplayName("Matricular personas con un valor incorrecto eleva una excepción")
+    public void matricular_PersonaNegativa_LanzaClubException() throws ClubException{
+        // Arrange
+        int plazasLibres = g.plazasLibres();
+        int Nmatricular1 = -1;
+        int Nmatricular2 = plazasLibres+1;
 
+        // Act + Assert
+        assertThrows(ClubException.class, () -> g.matricular(Nmatricular1));
+        assertThrows(ClubException.class, () -> g.matricular(Nmatricular2));
+    }
 
+    @Test
+    @DisplayName("El método toString genera la cadena esperada")
+    public void toString_CadenaEsperada_GeneraStringCorrecto() {
+        // Arrange
+        String resultadoEsperado = "(1 - Fútbol - 20.0 euros - P:10 - M:3)";
+
+        // Act + Assert
+        assertEquals(resultadoEsperado, g.toString());
+    }
+
+    @Test
+    @DisplayName("Dos grupos con el mismo código y actividad deben ser iguales según el equals")
+    public void equals_CompararGrupos_ComparaCorrectamente() throws ClubException {
+        // Arrange
+        Grupo g2 = new Grupo("1","Fútbol", 10, 3, 20);
+        Grupo g3 = new Grupo("2","Baloncesto", 10, 3, 20);
+        
+        // Act + Assert
+        assertTrue(g.equals(g2));
+        assertFalse(g.equals(g3));
+    }
+
+    @Test
+    @DisplayName("Dos grupos con el mismo código y actividad deben tener el mismo HashCode")
+    public void hashCode_CompararGrupos_ComparaCorrectamente() throws ClubException {
+        // Arrange
+        Grupo g2 = new Grupo("1","Fútbol", 10, 3, 20);
+        
+        // Act + Assert
+        assertEquals(g.hashCode(), g2.hashCode());
+    }
+    
 }
