9ef1b04a8333e50c55940e0081776dc84c9fad24
Eduardo <earizaabad@gmail.com>
2025-03-06 11:56:06 +0100

    a1

--- src/test/java/clubdeportivo/GrupoTest.java
+++ src/test/java/clubdeportivo/GrupoTest.java
@@ -1,5 +1,20 @@
 package clubdeportivo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GrupoTest {
 
+    private Grupo g;
+
+    @BeforeEach
+    public void init() throws ClubException {
+        g=new Grupo("1","Fútbol", 10, 0, 20);
+    }
+
+    @Test
+    public void Matricular_Grupo_Anyadematriculado(){
+        int p = g.plazasLibres();
+
+    }
 }
